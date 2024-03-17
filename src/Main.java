ca303046b9738a8459a4f3df8a2aabca2cdbe4d9
WizOscxr <157354842+WizOscxr@users.noreply.github.com>
2024-03-17 23:43:47 +0100

    Agregamos rol de protector

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class JuegoRoles {
     public static void main(String[] args) {
-        String[] jugadores = {"Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4", "Jugador 5", "Jugador 6", "Jugador 7", "Jugador 8"};
+        String[] jugadores = {"Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4",
+                             "Jugador 5", "Jugador 6", "Jugador 7", "Jugador 8"};
 
         String[] roles = {"Lobo", "Lobo", "Aldeano", "Vidente", "Protector", "Abuelo", "Perro-lobo", "Cazador"};
 
@@ -16,19 +17,18 @@ public class JuegoRoles {
 
         boolean lobosVivos = true;
         boolean aldeanosVivos = true;
-
-        boolean primeraNoche = true;
         boolean perroLoboSeleccionado = false;
-
 
         Scanner scanner = new Scanner(System.in);
 
         while (lobosVivos && aldeanosVivos) {
             System.out.println("\n--- Ronda de día ---");
+            // Lógica para el día
 
             System.out.println("\n--- Ronda de noche ---");
-
-            if (primeraNoche) {
+            // Lógica para la noche
+            if (!perroLoboSeleccionado) {
+                // Seleccionar el rol del Perro-lobo solo en la primera noche
                 System.out.println("Perro-lobo, ¿deseas ser lobo o aldeano? (lobo/aldeano)");
                 String decision = scanner.next();
                 if (decision.equalsIgnoreCase("lobo")) {
@@ -36,16 +36,25 @@ public class JuegoRoles {
                 } else {
                     roles[6] = "Aldeano";
                 }
-                primeraNoche = false;
                 perroLoboSeleccionado = true;
             } else {
-                if (perroLoboSeleccionado) {
-                    // Roles
-                } else {
-                    // Roles
-                }
+                // Lógica para los roles restantes en todas las noches excepto la primera
+                // Protector, Vidente y Lobos
+                System.out.println("Protector, selecciona a qué jugador deseas proteger:");
+                String jugadorProtegido = scanner.next();
+                System.out.println("El protector ha protegido a " + jugadorProtegido);
+
+                System.out.println("Lobos, discutan a qué jugador desean eliminar:");
+                String jugadorEliminado = scanner.next();
+                System.out.println("Los lobos han decidido eliminar a " + jugadorEliminado);
+
+                System.out.println("Vidente, ¿a qué jugador deseas investigar su rol?");
+                String jugadorInvestigado = scanner.next();
+                String rolInvestigado = roles[obtenerIndiceJugador(jugadores, jugadorInvestigado)];
+                System.out.println("El rol de " + jugadorInvestigado + " es: " + rolInvestigado);
             }
 
+            // Simulando el fin de la ronda
             System.out.println("¿Continuar? (s/n)");
             String continuar = scanner.next();
             if (continuar.equalsIgnoreCase("n")) {
@@ -53,6 +62,7 @@ public class JuegoRoles {
             }
         }
 
+        // Determinar el ganador
         if (lobosVivos) {
             System.out.println("¡Los lobos ganaron!");
         } else if (aldeanosVivos) {
@@ -66,9 +76,19 @@ public class JuegoRoles {
         Random random = new Random();
         for (int i = 0; i < jugadores.length; i++) {
             int indice = random.nextInt(jugadores.length);
+            // Intercambiamos los roles en los índices i e indice
             String temp = roles[i];
             roles[i] = roles[indice];
             roles[indice] = temp;
         }
+    }
+   
+    public static int obtenerIndiceJugador(String[] jugadores, String jugador) {
+        for (int i = 0; i < jugadores.length; i++) {
+            if (jugadores[i].equals(jugador)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
